4f9866b6862ebff688cafd6a18b8db006c2d4181
Haoroux <52127278+Haoroux@users.noreply.github.com>
2023-02-09 18:18:05 +0100

    Add files via upload

---
+++ osuForLeftHand.ino
@@ -0,0 +1,53 @@
+
+//The used librairies
+#include <Keyboard.h>
+
+//les pins des switchs
+int ButtonPin1 = 4;
+int ButtonPin2 = 3;
+int ButtonPin3 = 2;
+
+//états des switchs
+unsigned long ButtonState1 = 0;
+unsigned long ButtonState2 = 0;
+unsigned long ButtonState3 = 0;
+unsigned long TimeDelay = 50000;
+
+void setup() 
+{
+  pinMode(ButtonPin1, INPUT_PULLUP);
+  pinMode(ButtonPin2, INPUT_PULLUP);
+  pinMode(ButtonPin3, INPUT_PULLUP);
+
+  Keyboard.begin();
+}
+
+void loop() 
+{
+  ButtonState1 = !digitalRead(ButtonPin1);
+  ButtonState2 = !digitalRead(ButtonPin2); 
+  ButtonState3 = !digitalRead(ButtonPin3); 
+  Serial.println(ButtonState2);
+  Serial.println(ButtonState1);
+  
+  if (ButtonState1 == 1){
+    Keyboard.press('p');
+    ButtonState1 = micros();
+  } else if (micros()-ButtonState1 >= TimeDelay ) {
+    Keyboard.release('p');
+  }
+    
+  if (ButtonState2 == 1){
+    Keyboard.press('o');
+    ButtonState2 = micros();
+  } else if (micros()-ButtonState2 >= TimeDelay ) {
+    Keyboard.release('o');
+  }
+  
+    if (ButtonState3 == 1){
+    Keyboard.press('i');
+    ButtonState3 = micros();
+  } else if (micros()-ButtonState3 >= TimeDelay ) {
+    Keyboard.release('i');
+  }
+}
